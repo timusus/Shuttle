c0fff5b1ed4456a6ea7d6f5ede47b8805700df77
Till Kottmann <deletescape@outlook.com>
2017-02-27 22:29:24 +0100

    Replace explicite types with <>

--- app/src/main/java/com/simplecity/amp_library/ui/views/AnimatedExpandableListView.java
+++ app/src/main/java/com/simplecity/amp_library/ui/views/AnimatedExpandableListView.java
@@ -263,7 +263,7 @@ public class AnimatedExpandableListView extends ExpandableListView {
      * adapters used with AnimatedExpandableListView MUST extend this class.
      */
     public static abstract class AnimatedExpandableListAdapter extends BaseExpandableListAdapter {
-        private SparseArray<GroupInfo> groupInfo = new SparseArray<GroupInfo>();
+        private SparseArray<GroupInfo> groupInfo = new SparseArray<>();
         private AnimatedExpandableListView parent;
 
         private static final int STATE_IDLE = 0;
@@ -519,7 +519,7 @@ public class AnimatedExpandableListView extends ExpandableListView {
     }
 
     private static class DummyView extends View {
-        private List<View> views = new ArrayList<View>();
+        private List<View> views = new ArrayList<>();
         private Drawable divider;
         private int dividerWidth;
         private int dividerHeight;
