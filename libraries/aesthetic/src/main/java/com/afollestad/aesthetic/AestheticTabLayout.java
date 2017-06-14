56be139e882a5c1f2dcc932654253e53fef494a1
Tim Malseed <t.malseed@gmail.com>
2017-06-14 23:06:41 +1000

    Workaround for AestheticTabLayout ‘flickering’ issue

--- libraries/aesthetic/src/main/java/com/afollestad/aesthetic/AestheticTabLayout.java
+++ libraries/aesthetic/src/main/java/com/afollestad/aesthetic/AestheticTabLayout.java
@@ -2,6 +2,8 @@ package com.afollestad.aesthetic;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.annotation.ColorInt;
 import android.support.design.widget.TabLayout;
 import android.util.AttributeSet;
@@ -72,6 +74,47 @@ public class AestheticTabLayout extends TabLayout {
   @Override
   protected void onAttachedToWindow() {
     super.onAttachedToWindow();
+
+    /* Begin workaround
+
+      In some certain situations, the time between onAttachedToWindow and the color change observables
+      emitting is too much, and the view gets displayed momentarily using the original colors.
+
+      On top of that, Aesthetic seems to strip out or otherwise modify view 'theme' attributes,
+      so even if this TabLayout's parent Toolbar is using a 'dark' theme, the text for the TabItems still
+      comes out black.
+
+      As a workaround, we setup the colors in a blocking fashion, to ensure the correct colors are applied
+      when onAttachedToWindow is called.
+
+      Lastly, for some reason, the 'setTabTextColors' has to be called after a delay, or else it doesn't
+      seem to take effect. Hard to tell if that's because Aesthetic isn't producing the right colors immeditately,
+      or if it's something to do with TabLayout.
+
+      It's really not ideal to have blocking RX calls happening on the main thread, but until a proper solution
+      is found, this is the best I can do.
+
+      Note: To reproduce this issue, it seems the TabLayout needs to belong to a nested fragment.
+     */
+    final Integer primaryColor = Aesthetic.get()
+            .colorPrimary()
+            .blockingFirst();
+    ViewBackgroundAction.create(AestheticTabLayout.this).accept(primaryColor);
+
+    getHandler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+
+        ActiveInactiveColors activeInactiveColors = Aesthetic.get()
+                .colorIconTitle(Observable.just(primaryColor))
+                .blockingFirst();
+
+        setTabTextColors(
+                adjustAlpha(activeInactiveColors.inactiveColor(), UNFOCUSED_ALPHA),
+                activeInactiveColors.activeColor());
+      }
+    }, 5);
+    // End workaround
 
     bgModeSubscription =
         Aesthetic.get()
