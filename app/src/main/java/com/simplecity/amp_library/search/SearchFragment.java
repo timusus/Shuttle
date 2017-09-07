ebea712c098a429c0d9c31590a9055b3915dd5b0
Tim Malseed <t.malseed@gmail.com>
2017-09-08 07:46:21 +1000

    Remove unused import

--- app/src/main/java/com/simplecity/amp_library/search/SearchFragment.java
+++ app/src/main/java/com/simplecity/amp_library/search/SearchFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.util.Pair;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.SearchView;
