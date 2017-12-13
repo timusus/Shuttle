3d53512e794d079304836d6d986691ef6f352cc7
Tim Malseed <t.malseed@gmail.com>
2017-12-13 20:31:08 +1100

    Fixed a crash when deleting incl/excl items containing a ' in the path

--- app/src/main/java/com/simplecity/amp_library/sql/databases/InclExclHelper.java
+++ app/src/main/java/com/simplecity/amp_library/sql/databases/InclExclHelper.java
@@ -43,7 +43,7 @@ public class InclExclHelper {
 
     public static void deleteInclExclItem(InclExclItem inclExclItem) {
         DataManager.getInstance().getInclExclDatabase().delete(InclExclDbOpenHelper.TABLE_NAME,
-                InclExclDbOpenHelper.COLUMN_PATH + " = '" + inclExclItem.path + "'" +
+                InclExclDbOpenHelper.COLUMN_PATH + " = '" + inclExclItem.path.replaceAll("'", "\''") + "'" +
                         " AND " + InclExclDbOpenHelper.COLUMN_TYPE + " = " + inclExclItem.type);
     }
 
