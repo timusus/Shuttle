384bf18fa1638b64ce71ef1f486f0bcebdc207e5
Felix Salazar <fxsalazar@gmail.com>
2018-05-05 10:12:55 +0200

    Rollback direct instatiation of MultiSheetsEvents relays; Remove unnecesary Module.

--- app/src/main/java/com/simplecity/amp_library/dagger/component/AppComponent.java
+++ app/src/main/java/com/simplecity/amp_library/dagger/component/AppComponent.java
@@ -1,22 +1,22 @@
 package com.simplecity.amp_library.dagger.component;
 
-import com.simplecity.amp_library.dagger.module.ActivityModule;
-import com.simplecity.amp_library.dagger.module.AppModule;
-import com.simplecity.amp_library.dagger.module.DrawerModule;
-import com.simplecity.amp_library.dagger.module.ModelsModule;
-import com.simplecity.amp_library.dagger.module.MultiSheetModule;
+import com.simplecity.amp_library.dagger.module.*;
+import com.simplecity.amp_library.ui.views.multisheet.CustomMultiSheetView;
 import dagger.Component;
+
 import javax.inject.Singleton;
 
 @Singleton
 @Component(modules = {
         AppModule.class,
         ModelsModule.class,
-        MultiSheetModule.class,
         DrawerModule.class
 })
 
 public interface AppComponent {
 
     ActivityComponent plus(ActivityModule module);
+
+    void inject(CustomMultiSheetView target);
+
 }
