1a7ef5150b4e3323a527b5f33860860ff287d1cf
Felix Salazar <fxsalazar@gmail.com>
2018-05-05 09:53:26 +0200

    Remove JvmOverload annotation

--- app/src/main/java/com/simplecity/amp_library/ui/detail/playlist/PlaylistDetailPresenter.kt
+++ app/src/main/java/com/simplecity/amp_library/ui/detail/playlist/PlaylistDetailPresenter.kt
@@ -5,19 +5,15 @@ import com.simplecity.amp_library.model.Playlist
 import com.simplecity.amp_library.model.Song
 import com.simplecity.amp_library.playback.MediaManager
 import com.simplecity.amp_library.ui.presenters.Presenter
-import com.simplecity.amp_library.utils.ComparisonUtils
-import com.simplecity.amp_library.utils.LogUtils
-import com.simplecity.amp_library.utils.Operators
-import com.simplecity.amp_library.utils.PermissionUtils
-import com.simplecity.amp_library.utils.SortManager
+import com.simplecity.amp_library.utils.*
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
-import java.util.Random
+import java.util.*
 import java.util.concurrent.TimeUnit
 
-class PlaylistDetailPresenter @JvmOverloads constructor(private val mediaManager: MediaManager, private val playlist: Playlist) : Presenter<PlaylistDetailView>() {
+class PlaylistDetailPresenter constructor(private val mediaManager: MediaManager, private val playlist: Playlist) : Presenter<PlaylistDetailView>() {
 
     private var songs: MutableList<Song> = mutableListOf()
 
