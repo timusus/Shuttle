44728f326337f045f3a29f3e4abd223adebca5d8
Tim Malseed <t.malseed@gmail.com>
2018-08-13 20:54:36 +1000

    Revert "Added Firebase performance monitoring"
    FPM seems to introduce ANR's and various crashes.

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -72,6 +72,7 @@ object Dependencies {
     // In app purchases
     const val billing = "com.android.billingclient:billing:${Versions.billing}"
 
+
     object Plugins {
 
         object Versions {
@@ -82,7 +83,6 @@ object Dependencies {
             const val playPublisher = "1.2.2"
             const val gradleVersions = "0.20.0"
             const val playServices = "4.0.1"
-            const val firebasePlugin = "1.1.5"
         }
 
         const val android = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
@@ -92,7 +92,6 @@ object Dependencies {
         const val playPublisher = "com.github.triplet.gradle:play-publisher:${Versions.playPublisher}"
         const val gradleVersions = "com.github.ben-manes:gradle-versions-plugin:${Versions.gradleVersions}"
         const val playServices = "com.google.gms:google-services:${Versions.playServices}"
-        const val firebase = "com.google.firebase:firebase-plugins:${Versions.firebasePlugin}"
     }
 
     object Google {
@@ -103,7 +102,6 @@ object Dependencies {
             const val firebaseRemoteConfig = "16.0.0"
             const val chromecast = "15.0.1"
             const val constraintLayout = "1.1.0-beta5"
-            const val firebasePerf = "16.0.0"
         }
 
         const val cardView = "com.android.support:cardview-v7:${Versions.supportLib}"
@@ -119,7 +117,6 @@ object Dependencies {
         const val constraintLayout = "com.android.support.constraint:constraint-layout:${Versions.constraintLayout}"
         const val prefCompat = "com.android.support:preference-v7:${Versions.supportLib}"
         const val prefCompatv14 = "com.android.support:preference-v14:${Versions.supportLib}"
-        const val firebasePerf = "com.google.firebase:firebase-perf:${Versions.firebasePerf}"
     }
 
     object Square {
@@ -143,6 +140,7 @@ object Dependencies {
         const val retrofitGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofitGson}"
         const val sqlBrite = "com.squareup.sqlbrite2:sqlbrite:${Versions.sqlBrite}"
     }
+
 
     object Rx {
 
@@ -184,6 +182,7 @@ object Dependencies {
         const val traceur = "com.tspoon.traceur:traceur:${Versions.traceur}"
     }
 
+
     object Testing {
 
         object Versions {
@@ -221,6 +220,7 @@ object Dependencies {
         const val assertj = "org.assertj:assertj-core:${Versions.assertj}"
     }
 
+
     object Projects {
 
         // Cast Companion Library
@@ -243,6 +243,7 @@ object Dependencies {
     }
 
     object BuildPlugins {
+
         const val androidApplication = "com.android.application"
         const val androidLibrary = "com.android.library"
         const val kotlin = "kotlin-android"
@@ -253,7 +254,6 @@ object Dependencies {
         const val playPublisher = "com.github.triplet.play"
         const val gradleVersions = "com.github.ben-manes.versions"
         const val playServices = "com.google.gms.google-services"
-        const val firebasePerf = "com.google.firebase.firebase-perf"
     }
 }
 
