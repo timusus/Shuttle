d5c9a17bf5c37022148570a4e16aa2676f3ed57e
Tim Malseed <t.malseed@gmail.com>
2018-11-05 22:05:32 +1100

    Removed redundant call to CastContext.getSharedInstance()

--- app/src/main/java/com/simplecity/amp_library/ui/activities/BaseActivity.java
+++ app/src/main/java/com/simplecity/amp_library/ui/activities/BaseActivity.java
@@ -17,7 +17,6 @@ import android.widget.Toast;
 import com.afollestad.aesthetic.AestheticActivity;
 import com.android.billingclient.api.BillingClient;
 import com.android.billingclient.api.Purchase;
-import com.google.android.gms.cast.framework.CastContext;
 import com.greysonparrelli.permiso.Permiso;
 import com.simplecity.amp_library.R;
 import com.simplecity.amp_library.ShuttleApplication;
@@ -84,8 +83,6 @@ public abstract class BaseActivity extends AestheticActivity implements ServiceC
                 Toast.makeText(BaseActivity.this, R.string.iab_purchase_restored, Toast.LENGTH_SHORT).show();
             }
         });
-
-        CastContext castContext = CastContext.getSharedInstance(this);
 
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
